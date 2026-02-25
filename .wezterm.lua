c9a059de639291003138306f422ed29294fc033f
Christopher Lipe <chrislipe@pacific1.co>
2026-02-25 12:34:52 -0800

    Opacity testing, and update

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,8 +19,8 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 10
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
